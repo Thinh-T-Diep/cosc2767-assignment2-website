7a08af883c1b755e4a08a768b08e314e369e7a1c
Thinh <diepthethinh@gmail.com>
2022-09-02 21:54:53 +0700

    added the bash script for jenkins server and bash profile for path for Maven and Java

---
+++ jenkins-server-setup.sh
@@ -0,0 +1,15 @@
+
+
+#Installing Java
+sudo amazon-linux-extras install java-openjdk11 -y
+
+#Installing Maven
+cd /opt
+wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+tar -xvzf apache-maven-3.8.6-bin.tar.gz
+mv apache-maven-3.8.6 maven
+
+#Copying bash profile for maven and Java path
+cd ~
+cp /cosc2767-assignment2-website/.bash_profile .
+source .bash_profile
